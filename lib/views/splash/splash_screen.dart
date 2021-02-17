6f32519096765af8446d9cb06b4051d97131d0da
Mahmoud Al-Haroon <mahmoud.harooney.tooling@gmail.com>
2021-02-17 02:28:36 +0200

    Icon Launcher Added

--- lib/views/splash/splash_screen.dart
+++ lib/views/splash/splash_screen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 4), () {
-      Navigator.push(
+      Navigator.pushReplacement(
           context,
           MaterialPageRoute(
             builder: (context) => IPetLoginScreen(),
