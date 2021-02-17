78854a3475a2d794f1c28100b6c09e461329a3b6
Mahmoud Al-Haroon <mahmoud.harooney.tooling@gmail.com>
2021-02-17 16:21:07 +0200

    Updated tool bar and placed it in dashboard page

--- lib/views/dash_board_screen/dash_board_screen.dart
+++ lib/views/dash_board_screen/dash_board_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ipetmobile/constants/constants.dart';
 import 'package:ipetmobile/views/dash_board_screen/components/custom_grid_view_card.dart';
+import 'package:ipetmobile/widgets/app_drawer/app_drawer.dart';
+import 'package:ipetmobile/widgets/common/ipet_custom_icon.dart';
 import 'package:ipetmobile/widgets/common/ipet_custom_scaffold.dart';
+import 'package:ipetmobile/widgets/common/ipet_top_appbar.dart';
+import 'package:ipetmobile/widgets/common/my_custom_card.dart';
+import 'package:ipetmobile/widgets/default_image.dart';
 import 'package:ipetmobile/widgets/ipet_custom_sizedbox.dart';
 import 'package:ipetmobile/widgets/label.dart';
 
@@ -15,39 +21,33 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    var orientation = MediaQuery.of(context).orientation;
     return IPetCustomScaffold(
       ipKey: _scaffoldKey,
-      // iPetTopAppBar: IPetCustomTopBarWidget(
-      //   iPetFirstPart: MyCustomCard(
-      //     colour: Colors.transparent,
-      //     childCard: IPetCustomIcon(ipFontIc: FontAwesomeIcons.list),
-      //     myOnTap: () {
-      //       _scaffoldKey.currentState.openDrawer();
-      //     },
-      //   ),
-      //   iPetMiddlePart: [
-      //     DefaultImage(
-      //       image: 'assets/images/ipet_paw_img.png',
-      //     ),
-      //     Label(
-      //       text: 'Settings',
-      //     ),
-      //   ],
-      //   iPetLastPart: [],
-      // ),
-      // iPetTopAppBar: AppBar(),
-      // iPetTopAppBar: IPetCustomTopBarWidget(
-      //   iPetMiddlePart: [
-      //     DefaultImage(
-      //       image: 'assets/images/ipet_paw_img.png',
-      //     ),
-      //     Label(
-      //       text: 'DashBoard',
-      //       textColor: AppConst.kPrimaryColor,
-      //     ),
-      //   ],
-      //   iPetLastPart: [],
-      // ),
+      iPetTopAppBar: IPetCustomTopBarWidget(
+        // if (orientation == Orientation.portrait)
+        iPetFirstPart: orientation == Orientation.portrait
+            ? MyCustomCard(
+                colour: Colors.transparent,
+                childCard: IPetCustomIcon(ipFontIc: FontAwesomeIcons.list),
+                myOnTap: () {
+                  _scaffoldKey.currentState.openDrawer();
+                },
+              )
+            : null,
+        iPetMiddlePart: [
+          DefaultImage(
+            image: 'assets/images/ipet_paw_img.png',
+          ),
+          Label(
+            text: 'Dash-Board',
+            textColor: AppConst.kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ],
+        iPetLastPart: [],
+      ),
+      iPetDrawer: AppDrawer(),
       body: ListView(
         children: <Widget>[
           Padding(
--- lib/views/home/home_view_mobile.dart
+++ lib/views/home/home_view_mobile.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:ipetmobile/constants/constants.dart';
 import 'package:ipetmobile/viewmodels/home_viewmodel.dart';
 import 'package:ipetmobile/views/dash_board_screen/dash_board_screen.dart';
 import 'package:ipetmobile/widgets/app_drawer/app_drawer.dart';
 import 'package:ipetmobile/widgets/base_model_widget.dart';
-import 'package:ipetmobile/widgets/common/ipet_custom_icon.dart';
 import 'package:ipetmobile/widgets/common/ipet_custom_scaffold.dart';
-import 'package:ipetmobile/widgets/common/ipet_top_appbar.dart';
-import 'package:ipetmobile/widgets/common/my_custom_card.dart';
-import 'package:ipetmobile/widgets/default_image.dart';
-import 'package:ipetmobile/widgets/label.dart';
 
 class HomeMobilePortrait extends BaseModelWidget<HomeViewModel> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -18,33 +11,6 @@ class HomeMobilePortrait extends BaseModelWidget<HomeViewModel> {
   @override
   Widget build(BuildContext context, HomeViewModel model) {
     return IPetCustomScaffold(
-      iPetTopAppBar: IPetCustomTopBarWidget(
-        iPetFirstPart: MyCustomCard(
-          colour: Colors.transparent,
-          childCard: IPetCustomIcon(ipFontIc: FontAwesomeIcons.list),
-          myOnTap: () {
-            _scaffoldKey.currentState.openDrawer();
-          },
-        ),
-        iPetMiddlePart: [
-          DefaultImage(
-            image: 'assets/images/ipet_paw_img.png',
-          ),
-          Label(
-            text: 'Dash-Board',
-            textColor: AppConst.kPrimaryColor,
-            fontWeight: FontWeight.bold,
-          ),
-        ],
-        iPetLastPart: [],
-      ),
-      ipKey: _scaffoldKey,
-      iPetDrawer: AppDrawer(),
-      // iPetFAB: FloatingActionButton(
-      //   onPressed: () {
-      //     model.updateTitle();
-      //   },
-      // ),
       body: DashBoardScreen(),
     );
   }
